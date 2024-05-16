a862b4fd17d077faad35962b6168d12a157d1885
Emanuel Teixeira Martins <emanuel@itexto.com.br>
2024-05-16 00:36:07 -0300

    feat: initial implementation of the request to finish training

--- app_server.py
+++ app_server.py
@@ -1,14 +1,21 @@
 from crypt import methods
+from time import sleep
+import json
 from flask import Flask,jsonify,request
+import requests
 from flask_swagger_ui import get_swaggerui_blueprint
 import threading
 import subprocess
 
 process = None
-
+trainingUuid = None
 app = Flask(__name__)
 SWAGGER_URL="/swagger"
 API_URL="/static/swagger.json"
+IAAS_ENDPOINT_LOCAL="http://localhost:8080/api/v1/training"
+IAAS_ENDPOINT="http://api.iaas.emanuelm.dev/api/v1/training"
+actualTrainingUuid= None
+
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
@@ -34,6 +41,11 @@ def train():
     data = request.get_json()
     print(data)
 
+    global trainingUuid
+    trainingUuid = data.get("trainingUuid", "None")
+    if(trainingUuid == None):
+        return
+    
     server_ip = data.get("server_ip", "127.0.0.1")
     port = data.get("port", "3002")
     model_name = data.get("model_name", "alexnet")
@@ -46,6 +58,7 @@ def train():
 
     return jsonify({"Message": message})
 
+
 @app.route('/server-status', methods=['GET'])
 def check_server():
     if process and process.poll() is None:
@@ -53,12 +66,48 @@ def check_server():
     else:
         return jsonify({"status": "The server is twiddling its thumbs, waiting for action."}), 200
 
+
+
+def checkTrainingFinish():
+    global process
+    while(process is not None and process.poll() is None):
+        print("o treinamento ainda não terminou!")
+        sleep(5)
+
+    print("O treinamento terminou!")
+    performTrainingRequest()
+    
+
+def performTrainingRequest():
+    
+    file = open("./scripts/teste.pdf", 'rb')
+    
+    training_finish_dto = {
+        "timestamp": "2024-05-15T10:00:00Z",
+        "status": "success",
+        "data": "Training data",
+        "errors": []
+    }
+    
+    parts = {
+        'file': ('teste.pdf', file),
+        'request': ('json', json.dumps(training_finish_dto), 'application/json')
+    }
+    
+    global trainingUuid
+    requests.post(IAAS_ENDPOINT_LOCAL+"/finishTraining/"+trainingUuid, files=parts)
+    
+    
+    
 def run_fl_server(server_ip, port, model_name):
     global process 
     process = subprocess.Popen(["python3", "scripts/fl_server.py",
-                    "--port", port,
-                    "--model_name", model_name])
+                    "--port", str(port),
+                    "--model_name", str(model_name)])
+    
+    checkTrainingFinish()
+
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=8081)
