934d7f50498ac4c42a4e20190ed9351b14b7a628
victorfjansen <83790355+victorfjansen@users.noreply.github.com>
2024-05-31 17:48:49 -0300

    feat(server): paraleliza execução de servidores (#2)

--- app_server.py
+++ app_server.py
@@ -7,26 +7,20 @@ from flask_swagger_ui import get_swaggerui_blueprint
 import threading
 import subprocess
 
-process = None
-trainingUuid = None
-process = None
-app = Flask(__name__)
-SWAGGER_URL="/swagger"
-API_URL="/static/swagger.json"
-#O endereço ip no endpoint local se refere ao ip designado para a máquina do IAAS via VPN
-IAAS_ENDPOINT_LOCAL="http://10.255.0.2:8080/api/v1/training"
-IAAS_ENDPOINT="http://api.iaas.emanuelm.dev/api/v1/training"
-actualTrainingUuid= None
+from helpers import auxiliary
+from constants import endpoints, arguments_position
 
+process:list = []
+app = Flask(__name__)
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
+    endpoints.SWAGGER_URL,
+    endpoints.API_URL,
     config={
         'app_name': 'Access API'
     }
 )
-app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=endpoints.SWAGGER_URL)
 
 @app.route("/")
 def home():
@@ -36,48 +30,67 @@ def home():
     
 @app.route("/run/train", methods=["POST"])
 def train():
-    if process and process.poll() is None:
-        return jsonify({"status": "This server is already running a training. Please, wait for it become free to go again"}), 503
-    
-    print("Staring...")
     data = request.get_json()
-    print(data)
+    print('[INFO] JSON data from request', data)
 
-    global trainingUuid
-    trainingUuid = data.get("trainingUuid", "None")
-    if(trainingUuid == None):
-        return
+    port = data.get("port", "3002")
+    print('[INFO] getting training PORT', port)
+
+    global process
+    hasTrainingInPort = auxiliary.contains(process, lambda x: x.args[arguments_position.TRAINING_PORT_ARG_POSITION] == port)
+
+    if hasTrainingInPort:
+        return jsonify({"status": "[INFO] This server is already running a training. Please, wait for it become free to go again"}), 503
     
+    print("[INFO] Process staring...")
+
+    currentTrainingUuid = data.get("trainingUuid", None)
+
+    if(currentTrainingUuid == None):
+        print('[ERROR] trainingUuid not found on request body')
+        return
+
     numrounds = data.get("rounds", 3)
     server_ip = data.get("server_ip", "127.0.0.1")
-    port = data.get("port", "3002")
     model_name = data.get("model_name", "alexnet")
     
     # Executar fl_client.py em uma thread separada
     thread = threading.Thread(target=run_fl_server, args=(
-    server_ip, port, model_name, trainingUuid, numrounds))
+    server_ip, port, model_name, currentTrainingUuid, numrounds))
     thread.start()
 
-    message = f"Federated Learning Server started successfully."
+    message = f"[INFO] federated Learning Server started successfully."
 
     return jsonify({"Message": message})
 
 
 @app.route('/server-status', methods=['GET'])
 def check_server():
-    if process and process.poll() is None:
+    desirablePort = request.args.get('port')
+
+    if not desirablePort:
+        print('[INFO] The desirable port was not found in query params.')
+        return jsonify({"status": "The port was not found on queryString. Please, dispose the desirable port as a query param"}), 500  
+
+    global process
+    localProcess = auxiliary.contains(process, lambda x: x.args[arguments_position.TRAINING_PORT_ARG_POSITION] == desirablePort, False)
+
+    if not localProcess:
+        print('[INFO] Process not found in queue')
+        return jsonify({"status": "The desired process was not found in queue. Please try with other PORT"}), 500  
+
+    if localProcess and localProcess.poll() is None:
         return jsonify({"status": "The server is running a training."}), 503
-    else:
-        return jsonify({"status": "The server is twiddling its thumbs, waiting for action."}), 200
- 
-def performTrainingRequest():
     
+    return jsonify({"status": "The server is twiddling its thumbs, waiting for action."}), 200  
+
+def performTrainingRequest(port):
     file = open("./scripts/teste.pdf", 'rb')
     
     training_finish_dto = {
         "timestamp": "2024-05-15T10:00:00Z",
         "status": "success",
-        "data": "Training data",
+        "data": "Training datraining_finish_dtota",
         "errors": []
     }
     
@@ -87,26 +100,37 @@ def performTrainingRequest():
         'request': ('json', json.dumps(training_finish_dto), 'application/json')
     }
     
-    global trainingUuid
-    requests.post(IAAS_ENDPOINT_LOCAL+"/finishTraining/"+trainingUuid, files=parts)
-    
-def checkTrainingFinish():
     global process
-    while(process is not None and process.poll() is None):
-        print("-> o treinamento ainda não terminou!")
+    currentProcess = auxiliary.contains(process, lambda x: x.args[arguments_position.TRAINING_PORT_ARG_POSITION] == port, False)
+    currentUuid = currentProcess.args[arguments_position.TRAINING_UUID_ARG_POSITION]
+    
+    requests.post(endpoints.IAAS_ENDPOINT_LOCAL+"/finishTraining/"+currentUuid, files=parts)
+
+def checkTrainingFinish(port):
+    global process
+    localProcess = auxiliary.contains(process, lambda x: x.args[arguments_position.TRAINING_PORT_ARG_POSITION] == port, False)
+
+    if not localProcess:
+        print('[ERROR] process not found while checking if has finished')
+        return
+    
+    while(localProcess is not None and localProcess.poll() is None):
+        print("[INFO - training ", port, "] O treinamento da porta", port, "ainda não terminou!")
         sleep(10)
 
-    print("O treinamento terminou!")
-    performTrainingRequest()
+    print("[INFO - training ", port, "] O treinamento da porta ", port, " terminou!")
+    performTrainingRequest(port)
    
 def run_fl_server(server_ip, port, model_name, trainingUuid, rounds):
-    global process 
-    process = subprocess.Popen(["python3", "scripts/fl_server.py",
+    localProcess = subprocess.Popen(["python3", "scripts/fl_server.py",
                     "--port", str(port),
                     "--model_name", str(model_name),
                     "--trainingUuid", str(trainingUuid),
                     "--numRounds", str(rounds)])
-    checkTrainingFinish()
+        
+    global process 
+    process.append(localProcess)
+    checkTrainingFinish(port)
 
 
 if __name__=="__main__":
